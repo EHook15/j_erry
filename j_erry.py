457ee23081b276460a17413f0dd4a1137755c7ad
Eleanor Hook <kirby8151511@gmail.com>
2025-03-11 10:36:41 -0400

    he can blink, need to make smoother

--- j_erry.py
+++ j_erry.py
@@ -6,7 +6,6 @@ pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
 
-clock = pygame.time.Clock()
 running = True
 #colors
 faceColor = (201, 246, 255)
@@ -22,8 +21,10 @@ uwu = False
 sleepy = False
 headpat = False
 default = True #in use
+blink = False
 glitch = False
 startup = False
+ouch = False
 #def emotions
 
 while running:
@@ -31,7 +32,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if default:
+    if (random.randint(0, 10) == 1):
+        blink = True
+    if blink:
+        screen.fill((0, 0, 0))
         #points for polygons
         a = (125, 128)
         b = (185, 115)
@@ -42,7 +46,50 @@ while running:
         g = (306, 216)
         h = (253, 220)
         i = (77, 246)
-        j = (41, 284)
+        j = (61, 284)
+        k = (378, 270)
+        l = (358, 211)
+        pygame.draw.polygon(screen, faceColorBack, [a, b, c, d], backfacewidth) #eye 1 back
+        pygame.draw.polygon(screen, faceColorBack, [e, f, g ,h], backfacewidth) #eye 2 back
+        pygame.draw.line(screen, faceColorBack, i, j, backfacewidth) #mouth back
+        pygame.draw.line(screen, faceColorBack, j, k, backfacewidth)
+        pygame.draw.line(screen, faceColorBack, k, l, backfacewidth)
+        pygame.draw.polygon(screen, faceColor, [a, b, c, d], facewidth) #eye 1
+        pygame.draw.polygon(screen, faceColor, [e, f, g ,h], facewidth) #eye 2
+        pygame.draw.line(screen, faceColor, i, j, facewidth) #mouth
+        pygame.draw.line(screen, faceColor, j, k, facewidth)
+        pygame.draw.line(screen, faceColor, k, l, facewidth)
+        time.sleep(0.1)
+        screen.fill((0, 0, 0))
+        a = (90, 200)
+        b = (330, 190)
+        i = (77, 246)
+        j = (61, 284)
+        k = (378, 270)
+        l = (358, 211)
+        pygame.draw.line(screen, faceColorBack, a, b, backfacewidth) #blink line back
+        pygame.draw.line(screen, faceColorBack, i, j, backfacewidth) #mouth back
+        pygame.draw.line(screen, faceColorBack, j, k, backfacewidth)
+        pygame.draw.line(screen, faceColorBack, k, l, backfacewidth)
+        pygame.draw.line(screen, faceColor, a, b, facewidth) #blink line
+        pygame.draw.line(screen, faceColor, i, j, facewidth) #mouth
+        pygame.draw.line(screen, faceColor, j, k, facewidth)
+        pygame.draw.line(screen, faceColor, k, l, facewidth)
+        time.sleep(0.5)
+        blink = False
+    elif default:
+        screen.fill((0, 0, 0))
+        #points for polygons
+        a = (125, 128)
+        b = (185, 115)
+        c = (178, 220)
+        d = (129, 222)
+        e = (252, 105)
+        f = (307, 108)
+        g = (306, 216)
+        h = (253, 220)
+        i = (77, 246)
+        j = (61, 284)
         k = (378, 270)
         l = (358, 211)
         pygame.draw.polygon(screen, faceColorBack, [a, b, c, d], backfacewidth) #eye 1 back
@@ -56,6 +103,5 @@ while running:
         pygame.draw.line(screen, faceColor, j, k, facewidth)
         pygame.draw.line(screen, faceColor, k, l, facewidth)
     pygame.display.flip()
-    clock.tick(60) 
 
 pygame.quit()
